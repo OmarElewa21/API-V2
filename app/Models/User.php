0dcdcc3415376d3eedc1f06a85b12cbf7fde5a64
Hashtechs <hashtechs.stack@gmail.com>
2022-06-14 17:06:19 +0200

    --

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,7 +85,7 @@ class User extends Authenticatable
     }
 
     public function schoolManager(){
-        return $this->hasOne(schoolManager::class);
+        return $this->hasOne(SchoolManager::class);
     }
 
     public function getUserPermissionSet(){
